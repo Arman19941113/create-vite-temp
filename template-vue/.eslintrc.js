710a4cc756a9d01613bd7b51fdb58ed58962615d
Arman19941113 <td19941113@gmail.com>
2023-05-16 20:53:52 +0800

    chore: update template

--- template-vue/.eslintrc.js
+++ template-vue/.eslintrc.js
@@ -4,7 +4,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
@@ -12,4 +12,25 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
   },
+  rules: {
+    // vue
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          // since prettier is opinionated, we can't use 'never' here
+          void: 'any',
+          normal: 'any',
+          component: 'always',
+        },
+        svg: 'any',
+        math: 'any',
+      },
+    ],
+    // Eslint bugs
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  }
 }
